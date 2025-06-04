640574b24d3a2a8b4b837274e1a976dc47b792ff
SwapnilKatekar16 <swapnilkatekar16@gmail.com>
2025-06-04 17:02:54 +0530

    update 1

--- src/main/java/com/bookInformation/BookInfoFetcher.java
+++ src/main/java/com/bookInformation/BookInfoFetcher.java
@@ -1,11 +1,11 @@
 package com.bookInformation;
 
-import org.apache.catalina.util.URLEncoder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Scanner;
@@ -22,19 +22,25 @@ public class BookInfoFetcher implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter name of book");
-        String input=sc.nextLine();
-        String base_url="https://www.googleapis.com/books/v1/volumes?q="+input;
+        String input = sc.nextLine();
+        String base_url = "https://www.googleapis.com/books/v1/volumes?q=" + input;
         //String base_url=String.format("https://www.googleapis.com/books/v1/volumes?q=%s",input);
-       //String url = URLEncoder.encode(base_url,StandardCharsets.UTF_8);
-        GoogleBooksResponse json = restTemplate.getForObject(base_url, GoogleBooksResponse.class);
-        List<Volume> items = json.getItems();
-        if (items!=null&&!items.isEmpty()){
-            for (Volume item : items){
-                System.out.println("Title : "+item.getVolumeInfo().getTitle());
-                System.out.println("Publisher : "+item.getVolumeInfo().getPublisher());
+        // String url = URLEncoder.encode(base_url, StandardCharsets.UTF_8);
+        GoogleBooksResponse object = restTemplate.getForObject(base_url, GoogleBooksResponse.class);
+        List<Volume> items = object.getItems();
+        try {
+            if (!items.isEmpty() && items != null) {
+                for (Volume item : items) {
+                    System.out.println("title : " + item.getVolumeInfo().getTitle());
+                    System.out.println("Publisher : " + item.getVolumeInfo().getPublisher());
+                    System.out.println("############################################################################");
+                }
+            } else {
+                System.out.println("No data found");
             }
-        }else {
-            System.out.println("No data found");
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
